3c63d87779f26f1283d3aea18d274aff8168173b
AlexandraPaskannaya <paskannaya1998@yandex.by>
2021-05-19 14:39:08 +0300

    final redux and router

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,32 @@
-import {TasksPage} from "../pages";
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
+import {LoginPage, RegistrationPage, UsersPage, TasksPage} from "../pages";
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      
-      <TasksPage />
+      <Router>
+        <Route exact  path="/">
+          <LoginPage/>
+        </Route>
 
+        <Route path="/signUp">
+          <RegistrationPage/>
+        </Route>
+
+        <Route path="/tasks">
+          <TasksPage />
+        </Route>
+
+        <Route path="/users">
+          <UsersPage />
+        </Route>
+
+      </Router>
     </div>
   );
 }
